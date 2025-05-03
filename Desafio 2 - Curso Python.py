23ee9c7af2e8606a6bae83370ea9aabaf6cd3803
Eduarda <eduardaliz11@gmail.com>
2025-05-03 14:10:05 -0300

    Add files via upload

---
+++ Desafio 2 - Curso Python.py
@@ -0,0 +1,6 @@
+ano=input('Em que ano você nasceu?')
+mes=input('E qual mês você nasceu?')
+dia=input('Por fim, qual o dia que você nasceu?')
+
+print('Você faz aniversário no dia', dia, mes, ano)
+
