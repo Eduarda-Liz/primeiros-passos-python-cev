8cf3e7039137e12943becea9d96d21c0f2ba2742
Eduarda <eduardaliz11@gmail.com>
2025-05-07 21:13:06 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,8 @@
+inf = (input('Digite algo e descubra informações detalhadas: '))
+print('O tipo primitivo desse valor é', type(inf))
+print('A informação é um algarismo numérico?', inf.isnumeric())
+print('A informação está em caixa alta?', inf.isupper())
+print('A informação é uma letra?', inf.isalpha())
+print(f'A informação {inf} só tem espaço?', inf.isspace())
+#O "inf" é um objeto, que tem características e funcionalidades, atributos e metodos.
+
