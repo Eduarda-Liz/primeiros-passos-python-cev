e8e6e83282019ff3e0e8ce2707b05f0a19ba9a1d
Eduarda <eduardaliz11@gmail.com>
2025-05-17 18:20:48 -0300

    Add files via upload

---
+++ ex010.py
@@ -0,0 +1,4 @@
+din = float(input('Quanto dinheiro você tem na carteira? R$ '))
+dol = din/3.27
+print(f'Com {din:.2} reais você pode comprar {dol:.4} dolares americanos')
+
---
+++ ex011.py
@@ -0,0 +1,5 @@
+larg=float(input('Qual a largura da parede? '))
+alt=float(input('Qual a altura da parede? '))
+area=larg*alt
+lit=area/2
+print(f'Para pintar uma parede de {area} metros quadrados, será necessários {lit} litros de tinta')
